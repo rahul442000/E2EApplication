73f9ad27718ffa4ab1f4b97abe73764c0d0e440f
rahul442000 <rahulsoni442000@gmail.com>
2025-11-20 06:22:30 +0530

    Created Contraoller and Model for Books

---
+++ E2EApplication/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using E2EApplication.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E2EApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        static List<Book> books = new List<Book>
+        {
+            new Book { Id = 1, Title = "1984", Author = "George Orwell", yearPublished = "1949" },
+            new Book { Id = 2, Title = "To Kill a Mocking,ird", Author = "Harper Lee", yearPublished = "1960" },
+            new Book { Id = 3, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", yearPublished = "1925" },
+            new Book { Id = 4, Title = "Pride and Prejudice", Author = "Jane Austen", yearPublished = "1813" },
+            new Book { Id = 5, Title = "The Catcher in the Rye", Author = "J.D. Salinger", yearPublished = "1951" }
+        };
+    }
+}
--- E2EApplication/Models/Book.cs
+++ E2EApplication/Models/Book.cs
@@ -2,5 +2,9 @@
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string yearPublished { get; set; }
     }
 }
